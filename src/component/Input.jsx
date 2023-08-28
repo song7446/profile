b613ebde46f2b7e23128f6c81ac8a5648dcc562f
song7446 <songwon199943@gmail.com>
2023-08-28 13:52:00 +0900

    start

---
+++ src/component/Input.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Input() {
+  return (
+    <div className='Input'>
+      <h1>유저 프로필</h1>
+      <p>이름 : </p>
+      <p>Email : </p>
+      <p>나이 : </p>
+    </div>
+  )
+}
+
+export default Input
